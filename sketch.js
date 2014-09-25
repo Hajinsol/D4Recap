9ece8539effb9638e7b91afb2d1d8e41ce254fde
Hajinsol <jinsor5542@gmail.com>
2014-09-25 13:23:04 +0900

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,17 +1,12 @@
 function setup() {
-  createCanvas(640, 480);
-  background(01, 122, 183);
+  createCanvas(800, 600);
+  background(0);
   
   noFill();
-
+}
     stroke(255, 0, 0);
-  line(10, 10, 90, 10);
-
-  stroke(209, 122, 183);
-  fill(209, 122, 183);
-    rect(20, 20, 80, 50);
-
-    stroke(24, 234, 102);
-    fill(217, 224, 33);
-    ellipse(50, 50, 50, 50);
+  function draw()
+  {
+    background(0)
+    ellipse(400, 300, mouseX, mouseYs);
 }
